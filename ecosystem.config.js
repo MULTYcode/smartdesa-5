3a6956616708cb54013a12231790d03c3cb1b387
MULTYcode <mhdwasiman@gmail.com>
2025-06-24 10:20:03 +0700

    smartdesa-5 : update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'smartdesa-template-5',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3000',
+      args: 'start -p 3001',
       instances: 'max',
       exec_mode: 'cluster',
       env: {
